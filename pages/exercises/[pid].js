0731354d7e8814dcab69fd580d0a0910a741cc9d
Vitaly Liber <zenamax@gmail.com>
2020-04-25 17:07:12 +0300

    CEO for exercise was added

--- pages/exercises/[pid].js
+++ pages/exercises/[pid].js
@@ -4,6 +4,7 @@ import { fetchExercise } from "../../api/exercises";
 import CustomControlledCarousel from "../../components/CustomControlledCarousel";
 import React from "react";
 import Loading from "../../components/Loading";
+import Head from "next/head";
 
 function Packages({ data }) {
   const router = useRouter();
@@ -16,14 +17,38 @@ function Packages({ data }) {
       </>
     );
   }
+  const {
+    title,
+    desc,
+    images,
+    group: { title: groupTitle }
+  } = data;
   return (
     <div>
       <Header />
+      <Head>
+        <title>{title}</title>
+        <meta name="Description" content={desc} />
+        <link
+          rel="shortcut icon"
+          type="image/x-icon"
+          href="https://gym.casply.com/favicon.ico"
+        />
+        <link
+          rel="apple-touch-icon"
+          type="image/png"
+          href="https://gym.casply.com/favicon.png"
+        />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={desc} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={images[0] && images[0].url} />
+      </Head>
       <br />
       <div className="container">
         <div className="row">
           <div className="col d-lg-flex justify-content-lg-center">
-            <h3 className="mb-3">{data.title}</h3>
+            <h3 className="mb-3">{title}</h3>
           </div>
         </div>
       </div>
@@ -31,12 +56,10 @@ function Packages({ data }) {
         <div className="row justify-content-md-center">
           <div className="col-12 col-sm-12 col-md-12 col-lg-8">
             <div className="card mb-3">
-              <CustomControlledCarousel items={data.images} />
+              <CustomControlledCarousel items={images} />
               <div className="card-body">
-                <span className="badge badge-success mb-3">
-                  {data.group.title}
-                </span>
-                <p>{data.desc}</p>
+                <span className="badge badge-success mb-3">{groupTitle}</span>
+                <p>{desc}</p>
               </div>
             </div>
           </div>
